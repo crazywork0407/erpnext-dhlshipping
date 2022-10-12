a014c77c293589095beabcbdd56688f1ec108ab0
Andy Cenoz <crazywork0407@gmail.com>
2022-10-12 21:20:27 +0200

    - Changed - its module name as ERPNext DHL Shipping.

--- erpnext_dhlshipping/config/desktop.py
+++ erpnext_dhlshipping/config/desktop.py
@@ -1,10 +1,11 @@
 from frappe import _
 
+
 def get_data():
-	return [
-		{
-			"module_name": "ERPNext DHLShipping",
-			"type": "module",
-			"label": _("ERPNext DHLShipping")
-		}
-	]
+    return [
+        {
+            "module_name": "ERPNext DHL Shipping",
+            "type": "module",
+                    "label": _("ERPNext DHL Shipping")
+        }
+    ]
